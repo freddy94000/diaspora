a45afce06d98907f0e0d69d35fa976427904c5da
Freddy RAMOS <freddy.ramos94@gmail.com>
2020-12-01 21:43:46 +0100

    form newsletter

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\NewsletterMail;
+use App\Form\NewsletterMailType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,11 +13,27 @@ class HomeController extends AbstractController
 {
 
     /**
-     * @Route("/")
+     * @Route("/", name="default")
      */
-    public function indexAction(): Response
+    public function indexAction(Request $request): Response
     {
-        return $this->render('home/index.html.twig');
+        $email = new NewsletterMail();
+        $form = $this->createForm(NewsletterMailType::class, $email);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($email);
+            $em->flush();
+
+            return $this->redirectToRoute('default');
+        }
+
+        return $this->render('home/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
 }
