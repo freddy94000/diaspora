bbff8ff2fb702dd780fb5bb6bc3148059b154c2c
Freddy RAMOS <freddy.ramos94@gmail.com>
2020-12-06 20:14:07 +0100

    Correction

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,6 +13,9 @@ class HomeController extends AbstractController
 {
 
     /**
+     * @param Request $request
+     * @return Response
+     *
      * @Route("/", name="default")
      */
     public function indexAction(Request $request): Response
@@ -24,14 +27,46 @@ class HomeController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $email = $form->getData();
 
+            $newsletterMailRepository = $this->getDoctrine()->getRepository(NewsletterMail::class);
+            if ($newsletterMailRepository->findOneBy(['email' => $email->getEmail()])) {
+                $this->addFlash('warning', 'Vous êtes déjà inscrit à la newsletter');
+                return $this->redirectToRoute('default');
+            }
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($email);
             $em->flush();
+
+            $this->addFlash('success', 'Votre inscription à la newsletter s\'est déroulée avec succès');
 
             return $this->redirectToRoute('default');
         }
 
         return $this->render('home/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     *
+     * @Route("/newsletters", name="newsletters")
+     */
+    public function newslettersAction(Request $request): Response
+    {
+        $newsletterMailRepository = $this->getDoctrine()->getRepository(NewsletterMail::class);
+        $emails = $newsletterMailRepository->findAll();
+
+        $email = new NewsletterMail();
+        $form = $this->createForm(NewsletterMailType::class, $email, [
+            'action' => $this->generateUrl('default'),
+        ]);
+
+        $form->handleRequest($request);
+
+        return $this->render('home/newsletters.html.twig', [
+            'emails' => $emails,
             'form' => $form->createView(),
         ]);
     }
